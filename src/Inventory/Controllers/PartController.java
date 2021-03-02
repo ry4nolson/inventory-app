58b8c6f5942fc6a6f2c049de8d2198c37dfc2839
Ryan Olson <ryan.olson@fsgi.com>
2021-03-02 11:08:17 -0600

    start on parts UI

--- src/Inventory/Controllers/PartController.java
+++ src/Inventory/Controllers/PartController.java
@@ -1,7 +1,14 @@
 package Inventory.Controllers;
 
+import javafx.event.ActionEvent;
+
 /**
  * PartController handles the actions for the Part window
  */
 public class PartController {
+    public void save(ActionEvent actionEvent) {
+    }
+
+    public void cancel(ActionEvent actionEvent) {
+    }
 }
